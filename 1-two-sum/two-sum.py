bbe6d58fa2e60eed3c52f35f6cd705377d753fee
Bhavish srujan <156641338+bhavishsrujan01@users.noreply.github.com>
2025-06-27 08:48:20 +0530

    Time: 6 ms (50.49%) | Memory: 13.3 MB (48.32%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]  # Return the indices of the two numbers
-            seen[num] = i
-        return []  # Return an empty list if no pair is found
+        h = {}
+        for i in range(len(nums)):
+            h[nums[i]] = i 
+        for i in range(len(nums)):
+            y = target-nums[i]
+            if y in h and h[y]!=i:
+                return [i,h[y]]
+        return 
