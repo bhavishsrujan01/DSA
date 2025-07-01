2c60d2bc7b78718c49bf0d3fc74528287c194857
Bhavish srujan <156641338+bhavishsrujan01@users.noreply.github.com>
2025-07-01 08:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.94%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        k = 0
+        k = 0  # Pointer for the next position to insert a non-val element
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
-                k+=1
+                k += 1
         return k
