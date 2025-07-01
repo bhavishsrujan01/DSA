491240d27898eeec4bddfd5dbed5be7871f56466
Bhavish srujan <156641338+bhavishsrujan01@users.noreply.github.com>
2025-07-01 20:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (31.28%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def majorityElement(self, nums):
-        hashlist = {}
-        val = len(nums)/2
-        for i in range(len(nums)):
-            hashlist[nums[i]]=hashlist.get(nums[i],0)+1
-        for i,j in hashlist.items():
-            if j>val:
-                return i
-        return None
+        nums.sort()
+        return nums[len(nums)//2]
